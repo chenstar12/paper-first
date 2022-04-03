eb0e42abce530d26f1930c06e06d6f969f02febb
chenstar12 <756037142@qq.com>
2022-04-03 22:07:22 +0800

    config

--- models/deepconn.py
+++ models/deepconn.py
@@ -11,12 +11,8 @@ class DeepCoNN(nn.Module):
         self.opt = opt
         self.num_fea = 1  # DOC
 
-        self.user_word_embs = nn.Embedding(opt.vocab_size, opt.word_dim)  # vocab_size * 300
-        print('================================embedding========================================')
-        print('user_word_embs', self.user_word_embs.weight.data.shape)
-        self.item_word_embs = nn.Embedding(opt.vocab_size, opt.word_dim)  # vocab_size * 300
-        print('================================embedding========================================')
-        print('item_word_embs', self.item_word_embs.weight.data.shape)
+        self.user_word_embs = nn.Embedding(opt.vocab_size, opt.word_dim)  # torch.Size([50002, 300])
+        self.item_word_embs = nn.Embedding(opt.vocab_size, opt.word_dim)  # torch.Size([50002, 300])
         # 输出通道数 ---- opt.filters_num ---- 100； 卷积核大小 ---- (3,300)
         self.user_cnn = nn.Conv2d(1, opt.filters_num, (opt.kernel_size, opt.word_dim))
         self.item_cnn = nn.Conv2d(1, opt.filters_num, (opt.kernel_size, opt.word_dim))
@@ -30,30 +26,19 @@ class DeepCoNN(nn.Module):
     def forward(self, datas):
         _, _, uids, iids, _, _, user_doc, item_doc = datas
 
-        user_doc = self.user_word_embs(user_doc)
-        print('=============================================user_doc.shape============================')
-        print(user_doc.shape)
-        item_doc = self.item_word_embs(item_doc)
-        print('=============================================item_doc.shape============================')
-        print(item_doc.shape)
+        user_doc = self.user_word_embs(user_doc)  # 调用了Embedding类的forward函数 -> torch.Size([128, 500, 300])
+
+        item_doc = self.item_word_embs(item_doc)  # torch.Size([128, 500, 300])
 
         u_fea = F.relu(self.user_cnn(user_doc.unsqueeze(1))).squeeze(3)  # .permute(0, 2, 1)
-        print('u_fea.shape: ', u_fea.shape)
         i_fea = F.relu(self.item_cnn(item_doc.unsqueeze(1))).squeeze(3)  # .permute(0, 2, 1)
-        print('i_fea.shape: ', i_fea.shape)
 
         u_fea = F.max_pool1d(u_fea, u_fea.size(2)).squeeze(2)
-        print('maxpooled u_fea.shape: ', u_fea.shape)
         i_fea = F.max_pool1d(i_fea, i_fea.size(2)).squeeze(2)
-        print('maxpooled i_fea.shape: ', i_fea.shape)
 
         u_fea = self.dropout(self.user_fc_linear(u_fea))
-        print('fc  u_fea.shape: ', u_fea.shape)
 
         i_fea = self.dropout(self.item_fc_linear(i_fea))
-        print('fc i_fea.shape: ', i_fea.shape)
-        print('stack u:', torch.stack([u_fea], 1).shape)
-        print('stack i:', torch.stack([i_fea], 1).shape)
         return torch.stack([u_fea], 1), torch.stack([i_fea], 1)
 
     def reset_para(self):
@@ -68,6 +53,10 @@ class DeepCoNN(nn.Module):
 
         if self.opt.use_word_embedding:
             w2v = torch.from_numpy(np.load(self.opt.w2v_path))
+            print('==============================w2v===================================')
+            print(w2v)
+            print('==============================opt.w2v_path===================================')
+            print(np.load(self.opt.w2v_path).shape)
             if self.opt.use_gpu:
                 self.user_word_embs.weight.data.copy_(w2v.cuda())
                 self.item_word_embs.weight.data.copy_(w2v.cuda())
