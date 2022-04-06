112f908083b6c4ea10cd22b94cb62f0953c14c7e
chenstar12 <756037142@qq.com>
2022-04-06 12:14:21 +0800

    config

--- main.py
+++ main.py
@@ -34,12 +34,6 @@ def train(**kwargs):
         opt = getattr(config, kwargs['dataset'] + '_Config')()
     opt.parse(kwargs)
 
-    logger.info(
-        'model: ' + opt.model + '\n' + 'dataset: ' + opt.dataset + '\n' +
-        'batch_size:' + str(opt.batch_size) + '\n' + 'num_epochs: ' + str(opt.num_epochs) + '\n' +
-        'r_id_merge: ' + opt.r_id_merge + '\n' + 'ui_merge: ' + opt.ui_merge + '\n' +
-        'output: ' + opt.output)
-
     log_file_name = os.path.join(os.getcwd(), 'log',
                                  opt.dataset[:4] + '-' + opt.model + '.txt')
     logger.setLevel(level=logging.DEBUG)
@@ -48,6 +42,12 @@ def train(**kwargs):
     file_handler.setLevel(level=logging.INFO)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
+
+    logger.info(
+        'model: ' + opt.model + '\n' + 'dataset: ' + opt.dataset + '\n' +
+        'batch_size:' + str(opt.batch_size) + '\n' + 'num_epochs: ' + str(opt.num_epochs) + '\n' +
+        'r_id_merge: ' + opt.r_id_merge + '\n' + 'ui_merge: ' + opt.ui_merge + '\n' +
+        'output: ' + opt.output)
 
     random.seed(opt.seed)
     np.random.seed(opt.seed)
