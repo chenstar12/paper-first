ebaebd8bfa34caf79c753c7b429bd11b6da38e1d
chenstar12 <756037142@qq.com>
2022-04-07 15:28:44 +0800

    config

--- main-v.py
+++ main-v.py
@@ -35,7 +35,7 @@ def train(**kwargs):
         opt = getattr(config, kwargs['dataset'] + '_Config')()
     opt.parse(kwargs)
 
-    log_file_name = os.path.join('/content/drive/MyDrive/log', opt.dataset[:4] + '-' +
+    log_file_name = os.path.join('/content/log', opt.dataset[:4] + '-' +
                                  opt.model + '-' + str(time.strftime('%d%H%M')) + '.txt')
     logger.setLevel(level=logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - : %(message)s')
