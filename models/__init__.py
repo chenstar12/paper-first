42eb957f92b9687bd2fb00eb0bbd8b024df949b6
chenstar12 <756037142@qq.com>
2022-04-11 01:29:59 +0800

    config

--- models/__init__.py
+++ models/__init__.py
@@ -27,4 +27,6 @@ from .MSCI10 import MSCI10
 from .MSCI10Z import MSCI10Z
 from .MSCI10Z0 import MSCI10Z0
 from .MSCI11A import MSCI11A
+from .MSCI11B import MSCI11B
+from .MSCI11C import MSCI11C
 from .MSCI11 import MSCI11
