aa43d6772281a41d34df0e1eeffc6fa952aec322
chenstar12 <756037142@qq.com>
2022-04-11 00:08:54 +0800

    config

--- models/MSCI11A.py
+++ models/MSCI11A.py
@@ -13,7 +13,7 @@ class MSCI11A(nn.Module):
     def __init__(self, opt):
         super(MSCI11A, self).__init__()
         self.opt = opt
-        self.num_fea = 3  # 0,1,2 == id,doc,review
+        self.num_fea = 2  # 0,1,2 == id,doc,review
 
         self.user_net = Net(opt, 'user')
         self.item_net = Net(opt, 'item')
