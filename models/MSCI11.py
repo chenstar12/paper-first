6257a1077dabc79ea4d1a52d38fd9866b9ffe71c
chenstar12 <756037142@qq.com>
2022-04-10 17:13:12 +0800

    config

--- models/MSCI11.py
+++ models/MSCI11.py
@@ -80,8 +80,10 @@ class Net(nn.Module):
 
         u_i_id_emb = u_i_id_emb.view(-1, u_i_id_emb.size(1))  # [128,10,32]->[1280,32]
         if self.uori == 'user':
-            u_i_id_emb = F.leaky_relu_(self.cnn_u_id(u_i_id_emb.unsqueeze(1))).squeeze(
-                2)  # [1280,32]->[1280,1,32]->[1280,100,1] -> [1280,100]
+            u_i_id_emb = u_i_id_emb.unsqueeze(1)
+            u_i_id_emb = self.cnn_u_id(u_i_id_emb)
+            u_i_id_emb = F.leaky_relu_(u_i_id_emb)
+            u_i_id_emb = u_i_id_emb.squeeze(2)  # [1280,32]->[1280,1,32]->[1280,100,1] -> [1280,100]
         else:
             u_i_id_emb = F.leaky_relu_(self.cnn_i_id(u_i_id_emb.unsqueeze(1))).squeeze(2)
         u_i_id_emb = u_i_id_emb.view(-1, r_num, u_i_id_emb.size(2))  # [1280,100] -> [128,10,100]
