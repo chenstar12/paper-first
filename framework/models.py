23e7b4dabedded5432db2947404b89cef41471af
chenstar12 <756037142@qq.com>
2022-04-17 15:16:49 +0800

    config

--- framework/models.py
+++ framework/models.py
@@ -71,7 +71,9 @@ class Model(nn.Module):
                 print(polarity - subjectivity)
             elif self.opt.inference in ['PDA']:  # 调参：lambda2
                 tmp = polarity ** self.opt.lambda2
+                print(tmp)
                 tmp[torch.isnan(tmp)] = 0.8
+                print(tmp)
                 output = F.elu(output) * (tmp)
 
             return output
