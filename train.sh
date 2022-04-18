49b7f36db7deb931a2b123399bc485a669b7e0b0
chenstar12 <756037142@qq.com>
2022-04-18 23:36:38 +0800

    config

--- train.sh
+++ train.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
 
-!python3 main.py train --model=MSCI7 --num_fea=3 --dataset=Gourmet_Food_data
-!python3 main.py train --model=MSCI7A --num_fea=3 --dataset=Gourmet_Food_data
-!python main.py train --model=MSCI7B --num_fea=3 --dataset=Gourmet_Food_data
+for ((i=1;i<20;i++))
+do
+  # python xx.py --dataset=
+  echo $i
+done
