71d0304bdcc0d4e850cad5f68c7a549b1546a99a
chenstar12 <756037142@qq.com>
2022-04-19 19:42:46 +0800

    config

--- train-PD1.sh
+++ train-PD1.sh
@@ -3,4 +3,4 @@
 echo 'start train .......'
 python main.py train --model=MSCI0D --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.6 --inference='trans-PD1' --lambda1C=0.3 --lambda2C=0.01
 echo 'start train .......'
-python main.py train --model=MSCI0D --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.8 --inference='trans-PD1' --lambda1C=0.4 --lambda2C=0.01
+python main.py train --model=MSCI0D --num_fea=2 --dataset=Gourmet_Food_data --lambda1=1 --inference='trans-PD1' --lambda1C=0.4 --lambda2C=0.01
