faff1628c781a9608e9417d54ab5747ca938fce2
chenstar12 <756037142@qq.com>
2022-04-19 19:44:58 +0800

    config

--- train-PDA.sh
+++ train-PDA.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo 'start train .......'
-python main.py train --model=MSCI0D --num_fea=2 --dataset=Gourmet_Food_data --inference='trans-PDA' --lambda2=0.001 --lambda1C=0.05 --lambda2C=0.001
+python main.py train --model=MSCI0D --num_fea=2 --dataset=Gourmet_Food_data --inference='trans-PDA' --lambda2=0.05 --lambda1C=0.05 --lambda2C=0.001
 echo 'start train .......'
 python main.py train --model=MSCI0D --num_fea=2 --dataset=Gourmet_Food_data --inference='trans-PDA' --lambda2=0.005 --lambda1C=0.05 --lambda2C=0.01
