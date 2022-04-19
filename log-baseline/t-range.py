52612931d8a8bdc8b1b2ca0551fccc3918041bde
chenstar12 <756037142@qq.com>
2022-04-19 23:31:07 +0800

    config

--- log-baseline/t-range.py
+++ log-baseline/t-range.py
@@ -5,3 +5,7 @@ import numpy as np
 d = np.random.normal(size=(20))
 print(d)
 print(d[index])
+
+
+s= 'trans--'
+print(s[:5]=='trans')
--- train.sh
+++ train.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo 'only inference in eval..............................................................'
-echo 'start train .......'
-
 python main.py train --model=MSCI0D --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.2 --inference='trans-PD'
 python main.py train --model=MSCI0D --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.3 --inference='trans-PD'
 python main.py train --model=MSCI0D --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.5 --inference='trans-PD'
