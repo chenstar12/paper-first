28f8ddc5f465886c9068baff5dcc8033f9e56ab9
chenstar12 <756037142@qq.com>
2022-04-20 10:20:53 +0800

    config

--- train.sh
+++ train.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+echo 'sigmoid added'
+python main.py train --model=MSCI0D --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.3 --inference='trans-PD1'
+python main.py train --model=MSCI0D --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.4 --inference='trans-PD1'
+python main.py train --model=MSCI0D --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.5 --inference='trans-PD1'
+
 python main.py train --model=MSCI0D0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.4 --inference='trans-PD1'
 python main.py train --model=MSCI0D0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.4 --inference='trans-PD'
