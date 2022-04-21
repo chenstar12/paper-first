fc9f8ff3afcbdff9756d121051671caf25d95a61
chenstar12 <756037142@qq.com>
2022-04-21 20:03:11 +0800

    config

--- train-Kindle.sh
+++ train-Kindle.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python main.py train --model=MSCI0D --num_fea=2 --dataset=Kindle_Store_data --lambda1=0.5 --inference='trans-PD1'
+python main.py train --model=MSCI0D1 --num_fea=2 --dataset=Kindle_Store_data --lambda1=0.4 --inference='trans-PD1'
+python main.py train --model=MSCI0D1 --num_fea=2 --dataset=Kindle_Store_data --lambda1=0.5 --inference='trans-PD1'
+python main.py train --model=MSCI0D1 --num_fea=2 --dataset=Kindle_Store_data --lambda1=0.6 --inference='trans-PD1'
