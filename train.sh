8e0ea47522ac0210e1c92b546db67c8162bc1a11
chenstar12 <756037142@qq.com>
2022-04-22 23:34:12 +0800

    config

--- train.sh
+++ train.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-python main.py train --model=MSCI0D1 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.1 --inference='trans-PD1'
-python main.py train --model=MSCI0D1 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.3 --inference='trans-PD1'
-python main.py train --model=MSCI0D1 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.5 --inference='trans-PD1'
-python main.py train --model=MSCI0D1 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.7 --inference='trans-PD1'
-python main.py train --model=MSCI0D1 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.9 --inference='trans-PD1'
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.01 --inference='trans-PD1'
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.05 --inference='trans-PD1'
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.1 --inference='trans-PD1'
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.5 --inference='trans-PD1'
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.9 --inference='trans-PD1'
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=1.5 --inference='trans-PD1'
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=2.1 --inference='trans-PD1'
+
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.01
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.05
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.1
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.5
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.9
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=1.5
+python main.py train --model=MSCI0D1T0 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=2.1
