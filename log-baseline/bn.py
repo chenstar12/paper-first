c8523328d9695a7a17590eeb65adb905ecb80d2b
chenstar12 <756037142@qq.com>
2022-04-28 13:18:28 +0800

    config

--- log-baseline/bn.py
+++ log-baseline/bn.py
@@ -1,12 +1,12 @@
 import torch.nn as nn
 import torch
 
-x = torch.zeros([4,3],requires_grad=True)
+x = torch.zeros([4,3,5],requires_grad=True)
 print(x)
 
 nn.init.uniform_(x)
 print(x)
 
-bn=nn.BatchNorm1d(3,affine=True)
+bn=nn.BatchNorm1d(5,affine=True)
 x=bn(x)
 print(x)
