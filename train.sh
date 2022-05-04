feecae076b8671f7f024263317abab420ff21504
chenstar12 <756037142@qq.com>
2022-05-04 17:37:07 +0800

    config

--- train.sh
+++ train.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 python main.py train --model=MSCI0D1T5 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.01
-python main.py train --model=MSCI0D1T5 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.05
-python main.py train --model=MSCI0D1T5 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.1
-python main.py train --model=MSCI0D1T5 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.3
-python main.py train --model=MSCI0D1T5 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.5
-python main.py train --model=MSCI0D1T5 --num_fea=2 --dataset=Gourmet_Food_data  --inference='' --ei=''
-
 python main.py train --model=MSCI0D1T5A --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.01
-python main.py train --model=MSCI0D1T5A --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.05
-python main.py train --model=MSCI0D1T5A --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.1
-python main.py train --model=MSCI0D1T5A --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.3
-python main.py train --model=MSCI0D1T5A --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.5
-python main.py train --model=MSCI0D1T5A --num_fea=2 --dataset=Gourmet_Food_data  --inference='' --ei=''
-
 python main.py train --model=MSCI0D1T5B --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.01
+
+python main.py train --model=MSCI0D1T5 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.05
+python main.py train --model=MSCI0D1T5A --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.05
 python main.py train --model=MSCI0D1T5B --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.05
+
+python main.py train --model=MSCI0D1T5 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.1
+python main.py train --model=MSCI0D1T5A --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.1
 python main.py train --model=MSCI0D1T5B --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.1
-python main.py train --model=MSCI0D1T5B --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.3
-python main.py train --model=MSCI0D1T5B --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.5
+
+python main.py train --model=MSCI0D1T5 --num_fea=2 --dataset=Gourmet_Food_data  --inference='' --ei=''
+python main.py train --model=MSCI0D1T5A --num_fea=2 --dataset=Gourmet_Food_data  --inference='' --ei=''
 python main.py train --model=MSCI0D1T5B --num_fea=2 --dataset=Gourmet_Food_data  --inference='' --ei=''
+
+python main.py train --model=MSCI0D1T5 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.3
+python main.py train --model=MSCI0D1T5A --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.3
+python main.py train --model=MSCI0D1T5B --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.3
+
+python main.py train --model=MSCI0D1T5 --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.5
+python main.py train --model=MSCI0D1T5A --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.5
+python main.py train --model=MSCI0D1T5B --num_fea=2 --dataset=Gourmet_Food_data --lambda1=0.5
