a6599cb39ba1d3e7029ddd97baf652c2909ecd19
chenstar12 <756037142@qq.com>
2022-05-14 16:25:26 +0800

    config

--- train.sh
+++ train.sh
@@ -4,8 +4,3 @@ python main.py train --model=MSCI0D1T1 --num_fea=2  --dataset=Gourmet_Food_data 
 python main.py train --model=MSCI0D1T3 --num_fea=2  --dataset=Gourmet_Food_data --lambda1=0.1 --inference=trans-tanh --batch_size=1024
 python main.py train --model=MSCI0D1T5 --num_fea=2  --dataset=Gourmet_Food_data --lambda1=0.1 --inference=trans-tanh --batch_size=1024
 python main.py train --model=MSCI0D1T5B --num_fea=2  --dataset=Gourmet_Food_data --lambda1=0.1 --inference=trans-tanh --batch_size=1024
-
-python main.py train --model=MSCI0D1T1 --num_fea=2 --dataset=Gourmet_Food_data  --inference='' --ei='' --batch_size=1024
-python main.py train --model=MSCI0D1T3 --num_fea=2 --dataset=Gourmet_Food_data  --inference='' --ei='' --batch_size=1024
-python main.py train --model=MSCI0D1T5 --num_fea=2 --dataset=Gourmet_Food_data  --inference='' --ei='' --batch_size=1024
-python main.py train --model=MSCI0D1T5B --num_fea=2 --dataset=Gourmet_Food_data  --inference='' --ei='' --batch_size=1024
