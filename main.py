73ed0e92c9be2cf6895beb1cc2ca5430176547d5
chenstar12 <756037142@qq.com>
2022-05-24 13:52:08 +0800

    config

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ import os
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 
+
 def now():
     return str(time.strftime('%Y-%m-%d %H:%M:%S'))
 
@@ -269,9 +270,10 @@ def predict(model, data_loader, opt):
         x_min, x_max = X_tsne.min(0), X_tsne.max(0)
         X_norm = (X_tsne - x_min) / (x_max - x_min)  # 归一化
         plt.figure(figsize=(8, 8))
-        for i in range(X_norm.shape[0]):
-            plt.text(X_norm[i, 0], X_norm[i, 1], str(y[i]), color=plt.cm.Set1(y[i]),
-                     fontdict={'weight': 'bold', 'size': 9})
+        plt.scatter(X_norm[:, 0], X_norm[:, 1], c=y)
+        # for i in range(X_norm.shape[0]):
+        #     plt.text(X_norm[i, 0], X_norm[i, 1], str(y[i]), color=plt.cm.Set1(y[i]),
+        #              fontdict={'weight': 'bold', 'size': 9})
         plt.xticks([])
         plt.yticks([])
         plt.show()
