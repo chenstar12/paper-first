8dc1127f845ab4e86ea3ea4aa491d52ca754f5ef
chenstar12 <756037142@qq.com>
2022-05-24 15:26:58 +0800

    config

--- main.py
+++ main.py
@@ -263,8 +263,8 @@ def predict(model, data_loader, opt):
         X_tsne = tsne.fit_transform(data)
         x_min, x_max = X_tsne.min(0), X_tsne.max(0)
         X_norm = (X_tsne - x_min) / (x_max - x_min)  # 归一化
-        plt.figure(figsize=(8, 8))
-        plt.scatter(X_norm[:, 0], X_norm[:, 1], c=y)
+        # plt.figure(figsize=(8, 8))
+        plt.scatter(X_norm[:, 0], X_norm[:, 1], c=y, marker='x')
         # for i in range(X_norm.shape[0]):
         #     plt.text(X_norm[i, 0], X_norm[i, 1], str(y[i]), color=plt.cm.Set1(y[i]),
         #              fontdict={'weight': 'bold', 'size': 9})
